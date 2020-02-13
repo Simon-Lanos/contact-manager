286689cccf34b9f1e986e2ccabc448c24797b344
Simon LANOS <simon.lanos99@gmail.com>
2020-02-13 21:56:25 +0100

    chore(): add comments

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -4,17 +4,22 @@ function generateRandomString() {
 }
 
 function instantiateCollection(collection) {
-
+    //create the add button
     let addButton = document.createElement('button');
     addButton.type = 'button';
+    //add styling and content
     addButton.classList.add('btn');
     addButton.classList.add('btn-success');
     addButton.innerText = 'Add new';
 
+    //attach click handler to it
     addButton.addEventListener('click', function() {
+        //append a new form prototype at the end of the collection
+        //need to replace __name__ to avoid id collision
         collection.innerHTML += collection.dataset.prototype.replace('__name__', generateRandomString());
     });
 
+    //appends it to the begining of the collection
     collection.parentNode.insertBefore(addButton, collection);
 }
 
